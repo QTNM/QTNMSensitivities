7f16475323fc439a5a2d2015d23fab45bed756f8
Seb Jones <jonesseb100@gmail.com>
2023-01-16 14:41:01 +0000

    Updated location of fundamental constants

---
+++ src/include/FundamentalConstants.h
@@ -0,0 +1,25 @@
+/*
+  FundamentalConstants.h
+
+  Several constants of nature it's useful to keep track of
+*/
+
+#ifndef FUNDAMENTAL_CONSTANTS_H
+#define FUNDAMENTAL_CONSTANTS_H
+
+namespace sens
+{
+  // Electron rest mass in kilograms
+  inline constexpr double EMASS{9.1093837015e-31};
+
+  // Permeability of free space
+  inline constexpr double EPSILON0{8.8541878128e-12};
+
+  // Permeability of free space
+  inline constexpr double MU0{1.25663706212e-6};
+
+  // Elementary charge in coulombs
+  inline constexpr double QE{1.602176634e-19};
+}
+
+#endif
