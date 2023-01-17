fab2427fd6c70a7bd786b70fab6aace6eae489bd
Seb Jones <jonesseb100@gmail.com>
2023-01-17 14:46:34 +0000

    Library to do formatting Not sure this is really necessary

---
+++ src/Formatting/Formatting.cpp
@@ -0,0 +1,17 @@
+/// Formatting.cpp
+
+#include "Formatting.h"
+
+#include "TAxis.h"
+
+void sens::SetGraphAttr(TGraph *gr)
+{
+  gr->SetLineWidth(2);
+  gr->SetLineColor(kBlack);
+  gr->SetMarkerStyle(20);
+  gr->SetMarkerColor(kBlack);
+  gr->GetXaxis()->SetTitleSize(0.05);
+  gr->GetYaxis()->SetTitleSize(0.05);
+  gr->GetXaxis()->SetLabelSize(0.05);
+  gr->GetYaxis()->SetLabelSize(0.05);
+}
---
+++ src/Formatting/include/Formatting.h
@@ -0,0 +1,12 @@
+/*
+  Formatting.h
+*/
+
+#include "TGraph.h"
+
+namespace sens
+{
+  /// @brief 
+  /// @param gr 
+  void SetGraphAttr(TGraph *gr);
+}
