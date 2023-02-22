fa42da465d0593dbc4670db8c7deaaa04c95eaff
Seb Jones <jonesseb100@gmail.com>
2023-02-22 11:08:46 +0000

    New formatter using unique_ptr

--- src/Formatting/Formatting.cpp
+++ src/Formatting/Formatting.cpp
@@ -15,3 +15,15 @@ void sens::SetGraphAttr(TGraph *gr)
   gr->GetXaxis()->SetLabelSize(0.05);
   gr->GetYaxis()->SetLabelSize(0.05);
 }
+
+void sens::SetGraphAttr(std::unique_ptr<TGraph> &gr)
+{
+  gr->SetLineWidth(2);
+  gr->SetLineColor(kBlack);
+  gr->SetMarkerStyle(20);
+  gr->SetMarkerColor(kBlack);
+  gr->GetXaxis()->SetTitleSize(0.05);
+  gr->GetYaxis()->SetTitleSize(0.05);
+  gr->GetXaxis()->SetLabelSize(0.05);
+  gr->GetYaxis()->SetLabelSize(0.05);
+}
--- src/Formatting/include/Formatting.h
+++ src/Formatting/include/Formatting.h
@@ -9,4 +9,8 @@ namespace sens
   /// @brief 
   /// @param gr 
   void SetGraphAttr(TGraph *gr);
+
+  /// @brief Formats TGraphs to with desired label sizes
+  /// @param gr Unique pointer to TGraph
+  void SetGraphAttr(std::unique_ptr<TGraph> &gr);
 }
